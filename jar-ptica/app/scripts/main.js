3db2afb665fd9cb0a93e4cde25186afd5c30c8a3
abogdancode <abogdancode@gmail.com>
2017-02-02 00:23:07 +0300

    remake home

--- jar-ptica/app/scripts/main.js
+++ jar-ptica/app/scripts/main.js
@@ -1,17 +1,72 @@
-
 let homeBg = $("#home"),
     carouselItem = $('.item'),
     neededHeight = $(window).height(),
+    navLogo = $('#nav-logo'),
+    navLogoHeight = navLogo.height(),
+    translateHeight = navLogoHeight*5,
+    navH1 = $('#navH1'),
+    navLogoSmallText = $('#logoSmallText'),
+    myCarousel = $('#myCarousel'),
+    translateOff = false,
+    cycleOff = false,
     homeBgResize = (homeBg,neededHeight,carouselItem) => {
       homeBg.height(neededHeight);
       carouselItem.height(neededHeight);
-};
-$( window ).resize(()=> {
+    },
+    logoReplace = (st) => {
+      if(st<translateHeight){
+        navLogo.css({
+          "transform": "translate("+st/8*-1+"%,"+st/5*-1+"%"} );
+        navH1.css({
+          "transform": "translate("+st/10.2*-1+"%,0%"} );
+        navLogoSmallText.css({
+          "color": "transparent"} );
+      }else{
+        navLogo.css({
+          "display":"none"} );
+        navH1.css({
+          "transform": "translate(0%,0%",
+          "margin-left":"0"});
+        translateOff=true;
+      }
+    },
+    carouselSwitcher = (st) => {
+      switch (cycleOff) {
+        case true:
+          if(st<500){
+            $('#myCarousel').carousel("cycle");
+            cycleOff = false;
+            console.log("run");
+          }
+          break;
+        case false:
+          if(st>500){
+            $('#myCarousel').carousel("pause");
+            cycleOff=true;
+            console.log("pause");
+          }
+          break;
+      }
+    };
+$(window).scroll(function() {
+  let st = $(this).scrollTop();
+  if(!translateOff)
+    logoReplace(st);
+
+  carouselSwitcher(st);
+
+});
+$( window ).resize(function()  {
   homeBgResize(homeBg,neededHeight,carouselItem);
 });
-$( window ).ready(()=> {
+$( window ).ready(function() {
   homeBgResize(homeBg,neededHeight,carouselItem);
+  $('#myCarousel').carousel("pause");
   $('#myCarousel').carousel({
-    interval: 5000
+    interval: 100,
+    pause: false
   });
+
+
 });
+
